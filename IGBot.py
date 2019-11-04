11c8fcf138c4921c4123550fa07ee3c6efe8499b
Chariton Charitonidis <haritonch@yahoo.com>
2019-11-04 17:03:22 +0200

    new methods :)

--- IGBot.py
+++ IGBot.py
@@ -1,4 +1,4 @@
-from time import sleep
+from smartsleep import smartsleep
 import requests
 from selenium import webdriver
 
@@ -12,61 +12,137 @@ class IGBot:
         self.password = password
         self.driver = webdriver.Chrome('./chromedriver')
 
+
     def driver(self):
         return self.driver
 
+
     def login(self):
         self.driver.get('https://www.instagram.com/accounts/login/')
-        sleep(1) # you need to wait for react to build the front end
-        username_field = self.driver.find_elements_by_css_selector('input')[0]
-        password_field = self.driver.find_elements_by_css_selector('input')[1]
-        sleep(1)
+        smartsleep(2) # you need to wait for react to build the front end
+        username_field = self.driver.find_element_by_css_selector('input')
         username_field.send_keys(self.username + '\t' + self.password + '\n')
-        #password_field.send_keys(self.password + '\n')
-        sleep(2)
+        smartsleep(2)
 
 
     def visit(self, some_username):
         self.driver.get('https://www.instagram.com/' + some_username + '/')
+        smartsleep(2)
 
 
     def visitMyProfile(self):
         visit(self.username)
+        smartsleep(2)
 
 
     def follow(self, some_username):
+        if some_username == self.username:
+            print("You can't follow yourself! :P")
+            return
         self.visit(some_username)
-        sleep(2)
+        smartsleep(2)
         try:
             follow_button = self.driver.find_element_by_css_selector('button')
             if follow_button.text == 'Follow':
                 follow_button.click()
-                sleep(2)
+                smartsleep(2)
             else:
                 print('You are already following', some_username)
         except:
-            print("can't find the follow button")
+            print("I can't find the follow button")
             self.quit()
 
 
     def seePhoto(self, photo_id):
         self.driver.get('https://www.instagram.com/p/'+ photo_id +'/')
+        smartsleep(2)
 
 
     def like(self, photo_id):
         self.seePhoto(photo_id)
-        sleep(2)
+        try:
+            xpath = "//span[@aria-label='Like' or @aria-label='Unlike']"
+            heart = self.driver.find_element_by_xpath(xpath)
+            if heart.get_attribute('aria-label') == 'Unlike':
+                print("You have already liked the photo with id", photo_id)
+                smartsleep()
+            else:
+                heart.click()
+                smartsleep()
+        except:
+            print("can't find or click the heart button")
+            self.quit()
+
+    def unlike(self, photo_id):
+        self.seePhoto(photo_id)
         try:
             xpath = "//span[@aria-label='Like' or @aria-label='Unlike']"
             heart = self.driver.find_elements_by_xpath(xpath)[0]
             if heart.get_attribute('aria-label') == 'Unlike':
-                print('You have already liked the photo with id', photo_id)
-            else:
                 heart.click()
-                sleep(1)
+                smartsleep()
+            else:
+                print("You haven't liked the photo with id", photo_id)
+                smartsleep()
         except:
             print("can't find or click the heart button")
             self.quit()
+
+
+    def followersOf(self, some_username):
+        # returns a list with some followers of some_username
+        self.visit(some_username)
+        xpath = "//a[@href='/"  + some_username + "/followers/']"
+        followers_button = self.driver.find_element_by_xpath(xpath)
+        print(followers_button.text)
+        followers_button.click()
+        smartsleep(2)
+        xpath = "//a[@class and @title and @href]"
+        followers_elements = self.driver.find_elements_by_xpath(xpath)
+        followers = []
+        for follower_element in followers_elements:
+            followers.append(follower_element.text)
+        return followers
+
+
+    def myFollowers(self):
+        return followersOf(self.username)
+
+
+    def followeesOf(self, some_username):
+        self.visit(some_username)
+        xpath = "//a[@href='/"  + some_username + "/following/']"
+        following_button = self.driver.find_element_by_xpath(xpath)
+        following_button.click()
+        smartsleep(2)
+        xpath = "//a[@class and @title and @href]"
+        followees_elements = self.driver.find_elements_by_xpath(xpath)
+        followees = []
+        for followee_element in followees_elements:
+            followees.append(followee_element.text)
+        return followees
+
+
+    def likeRecentsOf(self, some_username):
+        self.visit(some_username)
+        xpath = "//a[@href]"
+        photo_elements = self.driver.find_elements_by_xpath(xpath)
+        photo_elements = list(filter(lambda element: '/p/' in element.get_attribute('href') , photo_elements))
+        photo_ids = list(map(lambda element: element.get_attribute('href')[28:-1], photo_elements))
+        for photo_id in photo_ids:
+            self.like(photo_id)
+        print('I liked', len(photo_ids),  'photos of', some_username)
+
+
+    def unlikeRecentsOf(self, some_username):
+        self.visit(some_username)
+        xpath = "//a[@href]"
+        photo_elements = self.driver.find_elements_by_xpath(xpath)
+        photo_elements = list(filter(lambda element: '/p/' in element.get_attribute('href'), photo_elements))
+        photo_ids = list(map(lambda element: element.get_attribute('href')[28:-1], photo_elements))
+        for photo_id in photo_ids:
+            self.unlike(photo_id)
+        print('I unliked the', len(photo_ids),  'most recent photos of', some_username)
 
 
     def quit(self):
--- main.py
+++ main.py
@@ -4,18 +4,17 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    if len(argv) == 2:
-        username = argv[0]
-        password = argv[1]
+    if len(argv) == 3:
+        username, password = argv[1:3]
     else:
         username = input('Username: ')
         password = getpass('Password: ')
 
-
     bot = IGBot(username, password)
 
     bot.login()
-
-    bot.like('B4WAnf5BbRl')
+    #bot.follow('hazoviolis') # that's me :P
+    #bot.like('B4a2BKep5h-') # that's a photo of mine :P
+    bot.getFollowersOf('hazoviolis')
 
     bot.quit()
